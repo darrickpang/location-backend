573e42e6453434eec85a9f455cd9253baf5d70f9
darrickpang <dkypang@yahoo.com>
2021-02-11 17:30:48 -0800

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/user_login', to: 'users#login'
+  get '/user_persist', to: 'users#persist'
 end
