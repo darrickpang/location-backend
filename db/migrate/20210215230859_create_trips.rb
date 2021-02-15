c97aa8807f41d421fbc8bcbc7401c59422881cca
darrickpang <dkypang@yahoo.com>
2021-02-15 15:13:38 -0800

    minor mistake changed

--- db/migrate/20210215230859_create_trips.rb
+++ db/migrate/20210215230859_create_trips.rb
@@ -3,9 +3,11 @@ class CreateTrips < ActiveRecord::Migration[6.0]
     create_table :trips do |t|
       t.string :name
       t.string :owner_uuid
-      t.string :owner_uuid
+      t.string :viewer_uuid
 
       t.timestamps
     end
+    add_index :trips, :viewer_uuid
+    add index :trips, :owner_uuid
   end
 end
