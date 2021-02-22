1c1fa6f3cb45342a3f8933137ecd2a53eaa726b1
darrickpang <dkypang@yahoo.com>
2021-02-22 14:10:36 -0800

    removed client

--- client/app/bundles/Trip/startup/registration.js
+++
@@ -1,9 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import NewTripContainer from '../containers/NewTripContainer';
-import ViewTripContainer from '../containers/ViewTripContainer';
-
-ReactOnRails.register({
-    NewTripContainer,
-    ViewTripContainer
-});
